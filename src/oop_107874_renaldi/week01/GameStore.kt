a9f0e72017f9e67895b2c59b396242343bcf3e91
Renaldi <renaldipanorama@gmail.com>
2026-02-09 21:59:54 +0700

    week01 complete

---
+++ src/oop_107874_renaldi/week01/GameStore.kt
@@ -0,0 +1,33 @@
+package oop_107874_renaldi.week01
+
+fun main() {
+    val gameTitle = "Elden Ring"
+    val price = 750_000
+    val userNote: String? = null
+
+    val finalPrice = calculateDiscount(price)
+
+    printReceipt(
+        title = gameTitle,
+        originalPrice = price,
+        finalPrice = finalPrice,
+        note = userNote
+    )
+}
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500_000) price * 80 / 100
+    else price * 90 / 100
+
+fun printReceipt(
+    title: String,
+    originalPrice: Int,
+    finalPrice: Int,
+    note: String?
+) {
+    println("=== SteamKW Receipt ===")
+    println("Game Title : $title")
+    println("Harga Asli : Rp $originalPrice")
+    println("Harga Akhir: Rp $finalPrice")
+    println("Catatan    : ${note ?: "Tidak ada catatan"}")
+}
