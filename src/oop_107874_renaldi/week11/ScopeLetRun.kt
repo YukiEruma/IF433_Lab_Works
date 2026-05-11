f7a533f05d39f91c43b3e27af9cf97276c970c13
Renaldi <renaldipanorama@gmail.com>
2026-05-11 16:39:21 +0700

    week11: implement let function for null safety

--- src/oop_107874_renaldi/week11/ScopeLetRun.kt
+++ src/oop_107874_renaldi/week11/ScopeLetRun.kt
@@ -1,2 +1,21 @@
 package oop_107874_renaldi.week11
 
+fun main() {
+    // CHECKPOINT 4 - let function
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    val length = name?.let {
+        println("Nama terdeteksi: $it")
+        it.length
+    } ?: 0
+    println("Panjang nama: $length")
+
+    // CHECKPOINT 5 - run function
+    println("\n=== TEST RUN FUNCTION ===")
+    val result = "Kotlin".run {
+        println("Memproses kata: ${this}")
+        this.length * 2
+    }
+    println("Hasil kalkulasi run: $result")
+}
