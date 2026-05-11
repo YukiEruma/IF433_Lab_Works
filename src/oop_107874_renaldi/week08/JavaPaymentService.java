3590e4cece961b2d8f03eb062475b64c1a2754ba
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:49:30 +0700

    week08: (task) create JavaPaymentService for payment interop

--- src/oop_107874_renaldi/week08/JavaPaymentService.java
+++ src/oop_107874_renaldi/week08/JavaPaymentService.java
@@ -1,4 +1,7 @@
 package oop_107874_renaldi.week08;
 
 public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
 }
