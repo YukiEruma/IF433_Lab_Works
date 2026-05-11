da5cf4ecd738038d0734d0f01591f15de75035ac
Renaldi <renaldipanorama@gmail.com>
2026-05-11 14:30:03 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_107874_renaldi/week03/Update Main.kt
+++ src/oop_107874_renaldi/week03/Update Main.kt
@@ -1,2 +1,29 @@
 package oop_107874_renaldi.week03
 
+fun main() {
+    val e = Employee("Budi")
+
+
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+
+    e.increasePerformance()
+
+
+    println("Pajak yang harus dibayar: ${e.tax}")
+
+    println("\n--- Test Weapon ---")
+    val sword = Weapon("Excalibur")
+    sword.damage = -50
+    sword.damage = 9999
+    println("Damage: ${sword.damage}, Tier: ${sword.tier}")
+
+    println("\n--- Test Player ---")
+    val player = Player("Renaldi")
+
+    player.addXp(50)
+    player.addXp(60)
+    println("Level sekarang: ${player.level}")
+}
