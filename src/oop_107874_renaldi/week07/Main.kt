bba4ade75672803bb58881f038b4ddc7651f8ae9
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:33:40 +0700

    week07: (trial) trigger private constructor error on NetworkClient

--- src/oop_107874_renaldi/week07/Main.kt
+++ src/oop_107874_renaldi/week07/Main.kt
@@ -1,2 +1,61 @@
 package oop_107874_renaldi.week07
 
+fun main() {
+
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
+
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegularUser("Alice", 22)
+    val reg2 = RegularUser("Alice", 22)
+    println(reg1)
+    println("Sama? ${reg1 == reg2}")
+
+
+    println("\n=== TEST DATA CLASS ===")
+    val data1 = DataUser("Alice", 22)
+    val data2 = DataUser("Alice", 22)
+    println(data1)
+    println("Sama? ${data1 == data2}")
+
+
+    val data3 = data1.copy(age = 23)
+    println("Hasil Copy: $data3")
+    val (userName, userAge) = data1
+    println("Destructured: $userName berumur $userAge")
+
+
+    println("\n=== TEST SEALED CLASS ===")
+    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
+    val uiMessage = when (response) {
+        is ApiResponse.Success -> "Tampilkan: ${response.data}"
+        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        ApiResponse.Loading -> "Tampilkan Spinner"
+    }
+    println(uiMessage)
+
+
+    println("\n=== TEST GAME MANAGER ===")
+    GameManager.startGame()
+    GameManager.startGame()
+
+
+    println("\n=== TEST RARITY & FACTORY ===")
+    println("Drop Chance Legendary: ${ItemRarity.LEGENDARY.dropChance}%")
+    val starterSword = Weapon.forgeStarterSword()
+    println("Senjata: ${starterSword.item.name} | Damage: ${starterSword.item.damage} | Rarity: ${starterSword.item.rarity} | Durability: ${starterSword.durability}")
+
+
+    println("\n=== TEST EVENT DISPATCH ===")
+    val upgradedItem = starterSword.item.copy(damage = 25)
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(upgradedItem))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
+}
