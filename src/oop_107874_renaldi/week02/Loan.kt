b77e08e1f96802903be6e44c1ab39c462ea5d5fc
Renaldi <renaldipanorama@gmail.com>
2026-05-11 14:09:11 +0700

    week02: (task 1) create Loan class with default args

--- src/oop_107874_renaldi/week02/Loan.kt
+++ src/oop_107874_renaldi/week02/Loan.kt
@@ -1,2 +1,14 @@
 package oop_107874_renaldi.week02
 
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1
+) {
+    fun calculateFine(): Int {
+        if (loanDuration > 3) {
+            return (loanDuration - 3) * 2000
+        }
+        return 0
+    }
+}
