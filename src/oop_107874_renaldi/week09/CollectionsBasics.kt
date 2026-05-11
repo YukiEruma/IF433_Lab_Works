cbe3099ce70dc345dcb6b929cda7066c5707f5e0
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:57:09 +0700

    week09: create immutable and mutable list

---
+++ src/oop_107874_renaldi/week09/CollectionsBasics.kt
@@ -0,0 +1,34 @@
+package oop_107874_renaldi.week09
+
+fun main() {
+    println("=== TEST LIST ===")
+    val frameworks: List<String> = listOf("Kotlin", "Java", "C++")
+    println("Immutable List: $frameworks")
+
+    val scores: MutableList<Int> = mutableListOf(85, 90)
+    scores.add(95)
+    scores[0] = 88
+    println("Mutable List: $scores")
+
+    println("\n=== TEST SET ===")
+    val uniqueNumbers = setOf(1, 2, 2, 3, 3, 4)
+    println("Unique Numbers (Set): $uniqueNumbers")
+
+    val activeUsers = mutableSetOf("UserA", "UserB")
+    activeUsers.add("UserC")
+    activeUsers.add("UserA")
+    println("Active Users: $activeUsers")
+
+    println("\n=== TEST MAP ===")
+    val studentGrades = mapOf(
+        "Alice" to "A",
+        "Bob" to "B",
+        "Charlie" to "A"
+    )
+    println("Nilai Bob: ${studentGrades["Bob"]}")
+
+    val inventory = mutableMapOf("Apples" to 50, "Bananas" to 30)
+    inventory["Oranges"] = 20
+    inventory["Apples"] = 45
+    println("Inventory: $inventory")
+}
