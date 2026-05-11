007c729fe2337622a08f4080c3d9f4760c0352c5
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:07:55 +0700

    week05: create Dosen class inheriting Pegawai

--- src/oop_107874_renaldi/week05/Dosen.kt
+++ src/oop_107874_renaldi/week05/Dosen.kt
@@ -1,2 +1,11 @@
 package oop_107874_renaldi.week05
 
+class Dosen(nama: String, val nidn: String) : Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS.")
+    }
+
+    fun mengajar() {
+        println("[$nama] sedang mengajar mahasiswa di kelas.")
+    }
+}
