457c6ecc8d910c0b6a8cea2d816f24f88830c064
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:49:29 +0700

    week08: implement smart cast in function

--- src/oop_107874_renaldi/week08/NotificationService.kt
+++ src/oop_107874_renaldi/week08/NotificationService.kt
@@ -1,2 +1,15 @@
 package oop_107874_renaldi.week08
 
+class NotificationService {
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile) {
+        if (user.email != null) {
+            sendEmail(emailAddress = user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
+}
