ae3beaef7ddf621ad257c831fffa1ec8cabd72d0
Renaldi <renaldipanorama@gmail.com>
2026-05-11 14:42:17 +0700

    week04: test Vehicle and Car in main

--- src/oop_107874_renaldi/week04/Main.kt
+++ src/oop_107874_renaldi/week04/Main.kt
@@ -1,2 +1,29 @@
 package oop_107874_renaldi.week04
 
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+
+    println("\n--- Testing ElectricCar ---")
+    val myElectricCar = ElectricCar(brand = "Tesla", numberOfDoors = 4, batteryCapacity = 85)
+    myElectricCar.accelerate()
+    myElectricCar.honk()
+    myElectricCar.openTrunk()
+
+    println("\n--- Testing Employee Hierarchy ---")
+    val manager = Manager(name = "Budi", baseSalary = 10000000)
+    manager.work()
+    println("Bonus Manager: ${manager.calculateBonus()}")
+
+    val developer = Developer(name = "Renaldi", baseSalary = 8000000, programmingLanguage = "Kotlin")
+    developer.work()
+    println("Bonus Developer: ${developer.calculateBonus()}")
+}
