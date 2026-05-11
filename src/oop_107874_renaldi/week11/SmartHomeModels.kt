7c201fdd92d14940dd4df0cfa78ace438ef756f2
Renaldi <renaldipanorama@gmail.com>
2026-05-11 16:39:22 +0700

    week11: (task) create SmartDevice data class

--- src/oop_107874_renaldi/week11/SmartHomeModels.kt
+++ src/oop_107874_renaldi/week11/SmartHomeModels.kt
@@ -1,2 +1,14 @@
 package oop_107874_renaldi.week11
 
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
+
+
+fun SmartDevice.diagnose(): String {
+    return "[DIAGNOSTIK] $name | Kategori: $category | Status: ${if (isOnline) "Online" else "Offline"} | Daya: $powerLoad Watt"
+}
