f1e588b7a53f8424b6fb6f369ed53e64d4e04d9b
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:21:57 +0700

    week06: (trial) trigger the diamond problem compiler error

--- src/oop_107874_renaldi/week06/Smartphone.kt
+++ src/oop_107874_renaldi/week06/Smartphone.kt
@@ -1,2 +1,9 @@
 package oop_107874_renaldi.week06
 
+class Smartphone : Camera, Phone {
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
+}
