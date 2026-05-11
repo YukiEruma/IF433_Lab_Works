8ef32105063f73ce5bc1fd9549874d19fa60d21c
Renaldi <renaldipanorama@gmail.com>
2026-05-11 14:42:29 +0700

    week04: (task 1) create ElectricCar class with final override

--- src/oop_107874_renaldi/week04/ElectricCar.kt
+++ src/oop_107874_renaldi/week04/ElectricCar.kt
@@ -1,2 +1,12 @@
 package oop_107874_renaldi.week04
 
+class ElectricCar(
+    brand: String,
+    numberOfDoors: Int,
+    val batteryCapacity: Int
+) : Car(brand, numberOfDoors) {
+
+    final override fun accelerate() {
+        println("$brand berakselerasi dalam sunyi. Kapasitas baterai: $batteryCapacity%.")
+    }
+}
