7a00bbda3d2dd0db4e540d633ad0eb83509428a8
Renaldi <renaldipanorama@gmail.com>
2026-05-11 14:28:28 +0700

    week03: (task 2) create Player class with private xp

--- src/oop_107874_renaldi/week03/Player.kt
+++ src/oop_107874_renaldi/week03/Player.kt
@@ -1,2 +1,22 @@
 package oop_107874_renaldi.week03
 
+class Player(val username: String) {
+
+    private var xp: Int = 0
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount <= 0) {
+            println("ERROR: XP yang ditambahkan harus positif!")
+            return
+        }
+        val levelSebelum = level
+        xp += amount
+        println("$username mendapat $amount XP. Total XP: $xp")
+        if (level > levelSebelum) {
+            println("Level Up! Selamat $username naik ke level $level")
+        }
+    }
+}
