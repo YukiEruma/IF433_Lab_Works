f868bcc67a0e23b321edbf2b9ba0f08c843d3a3a
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:21:57 +0700

    week06: (task) create SmartLamp

--- src/oop_107874_renaldi/week06/SmartLamp.kt
+++ src/oop_107874_renaldi/week06/SmartLamp.kt
@@ -1,2 +1,12 @@
 package oop_107874_renaldi.week06
 
+
+class SmartLamp(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnOn() {
+        println("[$name] Lampu menyala.")
+    }
+
+    override fun turnOff() {
+        println("[$name] Lampu dimatikan.")
+    }
+}
