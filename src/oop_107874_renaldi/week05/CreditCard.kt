4e271b3df4c02d5e643f03a0a142dc755cd820a2
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:09:57 +0700

    week05: (task 2) create CreditCard class

--- src/oop_107874_renaldi/week05/CreditCard.kt
+++ src/oop_107874_renaldi/week05/CreditCard.kt
@@ -1,2 +1,14 @@
 package oop_107874_renaldi.week05
 
+class CreditCard(accountName: String, val limit: Double) : PaymentMethod(accountName) {
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("[$accountName] Pembayaran Credit Card Rp$amount berhasil. Total terpakai: Rp$usedAmount")
+        } else {
+            println("[$accountName] Transaksi ditolak. Melebihi limit kartu.")
+        }
+    }
+}
