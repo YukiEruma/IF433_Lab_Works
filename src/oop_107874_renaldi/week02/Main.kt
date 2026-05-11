81040dd299d55e94a294aff34ed0e8fe124d99b3
Renaldi <renaldipanorama@gmail.com>
2026-05-11 14:06:09 +0700

    week02: implement user input and pre-validation

--- src/oop_107874_renaldi/week02/Main.kt
+++ src/oop_107874_renaldi/week02/Main.kt
@@ -1,2 +1,37 @@
 package oop_107874_renaldi.week02
 
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+    scanner.nextLine()
+
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+    } else {
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine()
+
+        if (type == 1) {
+            print("Masukkan Jurusan: ")
+            val major = scanner.nextLine()
+            val s1 = Student(name, nim, major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+            println("Status: Pendaftaran Selesai.")
+        } else if (type == 2) {
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+            println("Status: Pendaftaran Selesai.")
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
+    }
+}
