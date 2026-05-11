d6d0660a7c411247b551ec165c2ca93752b6f283
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:33:41 +0700

    week07: (task) implement factory pattern in WeaponForge

--- src/oop_107874_renaldi/week07/WeaponForge.kt
+++ src/oop_107874_renaldi/week07/WeaponForge.kt
@@ -1,2 +1,19 @@
 package oop_107874_renaldi.week07
 
+class Weapon private constructor(val item: GameItem, val durability: Int) {
+    companion object {
+        fun forgeStarterSword(): Weapon {
+            return Weapon(
+                item = GameItem("Pedang Kayu Bapuk", 5, ItemRarity.COMMON),
+                durability = 50
+            )
+        }
+
+        fun forgeEpicSword(): Weapon {
+            return Weapon(
+                item = GameItem("Pedang Naga Api", 500, ItemRarity.EPIC),
+                durability = 200
+            )
+        }
+    }
+}
