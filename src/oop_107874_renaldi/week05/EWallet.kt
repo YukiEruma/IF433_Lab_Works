43f19dd60f8a87c7917a9da14533e77b262d6e26
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:09:48 +0700

    week05: (task 2) create EWallet class

--- src/oop_107874_renaldi/week05/EWallet.kt
+++ src/oop_107874_renaldi/week05/EWallet.kt
@@ -1,4 +1,17 @@
 package oop_107874_renaldi.week05
 
-class EWallet {
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("[$accountName] Pembayaran EWallet Rp$amount berhasil. Sisa saldo: Rp$balance")
+        } else {
+            println("[$accountName] Saldo tidak cukup. Saldo saat ini: Rp$balance")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("[$accountName] Top up Rp$amount berhasil. Saldo sekarang: Rp$balance")
+    }
 }
