4d06b921603a637fc89d1a11427ced3d9b1e3292
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:21:57 +0700

    week06: (task) create SmartHomeHub logic

--- src/oop_107874_renaldi/week06/SmartHomeHub.kt
+++ src/oop_107874_renaldi/week06/SmartHomeHub.kt
@@ -1,2 +1,31 @@
 package oop_107874_renaldi.week06
 
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("Perangkat '${device.name}' ditambahkan ke Hub.")
+    }
+
+    fun turnOffAllSwitches() {
+        println("\n--- Mematikan semua perangkat ---")
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode() {
+        println("\n--- Mode Keamanan Aktif ---")
+        for (device in devices) {
+            if (device is Recordable) {
+                device.startRecord()
+            }
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
+}
