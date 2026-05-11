f8e4c66542c1e98c89c97f7e45b2c1e24bc9064e
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:33:41 +0700

    week07: (task) create ItemRarity enum

--- src/oop_107874_renaldi/week07/GameModels.kt
+++ src/oop_107874_renaldi/week07/GameModels.kt
@@ -1,2 +1,15 @@
 package oop_107874_renaldi.week07
 
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(40),
+    RARE(20),
+    EPIC(5),
+    LEGENDARY(1)
+}
+
+data class GameItem(
+    val name: String,
+    val damage: Int,
+    val rarity: ItemRarity
+)
