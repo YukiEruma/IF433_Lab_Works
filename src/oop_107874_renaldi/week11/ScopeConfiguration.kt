d5c0b51db3f36ecec7feefdd59ab5454b9e137d6
Renaldi <renaldipanorama@gmail.com>
2026-05-11 16:39:21 +0700

    week11: create user class for scope function tests

--- src/oop_107874_renaldi/week11/ScopeConfiguration.kt
+++ src/oop_107874_renaldi/week11/ScopeConfiguration.kt
@@ -1,2 +1,35 @@
 package oop_107874_renaldi.week11
 
+
+data class User(var name: String = "", var age: Int = 0)
+
+fun main() {
+
+    println("=== TEST APPLY ===")
+    val user = User().apply {
+        name = "Alex"
+        age = 25
+    }
+    println(user)
+
+
+    println("\n=== TEST ALSO ===")
+    val numbers = mutableListOf(1, 2, 3)
+    numbers.also {
+        println("Log Sebelum ditambah: $it")
+    }.add(4)
+    println("Setelah ditambah: $numbers")
+
+
+    println("\n=== TEST WITH ===")
+    with(user) {
+        println("User Detail -> Nama: $name, Umur: $age")
+    }
+
+
+    val newUser = User("Budi", 20).apply {
+        age = 21 // Mengubah umur
+    }.also {
+        println("User baru berhasil dibuat: $it")
+    }
+}
