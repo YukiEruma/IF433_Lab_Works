512169eeaa14dc5c223bc44ff19ab331f9de27a2
Renaldi <renaldipanorama@gmail.com>
2026-05-11 14:43:11 +0700

    week04: (task 2) create Manager class overriding Employee

--- src/oop_107874_renaldi/week04/Manager.kt
+++ src/oop_107874_renaldi/week04/Manager.kt
@@ -1,2 +1,12 @@
 package oop_107874_renaldi.week04
 
+class Manager(name: String, baseSalary: Int) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang memimpin rapat divisi.")
+    }
+
+    override fun calculateBonus(): Int {
+        return super.calculateBonus() + 500000
+    }
+}
