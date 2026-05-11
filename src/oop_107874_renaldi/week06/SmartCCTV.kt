503ed99ac343ad0731c77ae9e8c5567bc6a0342c
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:21:57 +0700

    week06: (task) create SmartCCTV with compound interfaces

--- src/oop_107874_renaldi/week06/SmartCCTV.kt
+++ src/oop_107874_renaldi/week06/SmartCCTV.kt
@@ -1,2 +1,16 @@
 package oop_107874_renaldi.week06
 
+class SmartCCTV(override val id: String, override val name: String) : SmartDevice, Switchable, Recordable {
+    override fun turnOn() {
+        println("[$name] CCTV menyala.")
+        startRecord()
+    }
+
+    override fun turnOff() {
+        println("[$name] CCTV dimatikan.")
+    }
+
+    override fun startRecord() {
+        println("[$name] Perekaman dimulai.")
+    }
+}
