cbb21c4d718a9b2cb0e82fe02fa7d5005d91ceec
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:33:40 +0700

    week07: create AppState enum

--- src/oop_107874_renaldi/week07/SystemStates.kt
+++ src/oop_107874_renaldi/week07/SystemStates.kt
@@ -1,2 +1,11 @@
 package oop_107874_renaldi.week07
 
+enum class AppState {
+    STARTING, RUNNING, STOPPED
+}
+
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
+}
