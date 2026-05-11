3de727e2259010203a1a97161b07a8a9e89b175e
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:21:57 +0700

    week06: (task) create SmartSpeaker

--- src/oop_107874_renaldi/week06/SmartSpeaker.kt
+++ src/oop_107874_renaldi/week06/SmartSpeaker.kt
@@ -1,2 +1,15 @@
 package oop_107874_renaldi.week06
 
+class SmartSpeaker(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnOn() {
+        println("[$name] Speaker menyala.")
+    }
+
+    override fun turnOff() {
+        println("[$name] Speaker dimatikan.")
+    }
+
+    fun playMusic(song: String) {
+        println("[$name] Memutar lagu $song dari Spotify.")
+    }
+}
