7801547ea284c99482231c909b2da8a3455a682e
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:49:30 +0700

    week08: (task) create sealed class Product and data classes

--- src/oop_107874_renaldi/week08/EcommerceModels.kt
+++ src/oop_107874_renaldi/week08/EcommerceModels.kt
@@ -1,2 +1,6 @@
 package oop_107874_renaldi.week08
 
+sealed class Product {
+    data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
+    data class Clothing(val id: String, val name: String, val size: String) : Product()
+}
