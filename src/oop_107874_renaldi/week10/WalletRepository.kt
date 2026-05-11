66fc508577a97ca58b2e131d77a36fa7d72ccdf1
Renaldi <renaldipanorama@gmail.com>
2026-05-11 16:17:21 +0700

    week10: (task) implement generic WalletRepository

--- src/oop_107874_renaldi/week10/WalletRepository.kt
+++ src/oop_107874_renaldi/week10/WalletRepository.kt
@@ -1,2 +1,21 @@
 package oop_107874_renaldi.week10
 
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+
+    fun count(): Int = items.size
+
+    // CHECKPOINT 14 - Search/filter dengan predicate
+    fun search(predicate: (T) -> Boolean): List<T> {
+        return items.filter(predicate)
+    }
+}
