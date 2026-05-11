d1e5d0a43cd218c53d43e5e5a6e3c0d526d78bb5
Renaldi <renaldipanorama@gmail.com>
2026-05-11 14:28:07 +0700

    week03: (task 1) create Weapon class with validation

--- src/oop_107874_renaldi/week03/Weapon.kt
+++ src/oop_107874_renaldi/week03/Weapon.kt
@@ -1,2 +1,24 @@
 package oop_107874_renaldi.week03
 
+class Weapon(val name: String) {
+
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("WARNING: Damage tidak boleh negatif! Nilai tidak diubah.")
+                // field tidak diubah, tetap nilai lama
+            } else if (value > 1000) {
+                println("WARNING: Damage terlalu besar! Dipaksa menjadi 1000.")
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
