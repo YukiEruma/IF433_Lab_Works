0b5994d34f854d35c49d04360e49b47c60413ed2
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:21:57 +0700

    week06: create Smartwatch with multiple implementations

--- src/oop_107874_renaldi/week06/Smartwatch.kt
+++ src/oop_107874_renaldi/week06/Smartwatch.kt
@@ -1,2 +1,15 @@
 package oop_107874_renaldi.week06
 
+class Smartwatch : Watch(), BluetoothConnectable, Rechargeable {
+    override fun showTime() {
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+
+    override fun connectToBluetooth() {
+        println("Mencari perangkat HP di sekitar untuk pairing...")
+    }
+
+    override fun chargeBattery() {
+        println("Mengisi daya menggunakan charger magnetik 15W.")
+    }
+}
