270359b381d0717c1d296886139247e502b8e9b3
Renaldi <renaldipanorama@gmail.com>
2026-05-11 15:21:57 +0700

    week06: create Camera and Phone interfaces with default methods

--- src/oop_107874_renaldi/week06/GadgetInterfaces.kt
+++ src/oop_107874_renaldi/week06/GadgetInterfaces.kt
@@ -1,2 +1,9 @@
 package oop_107874_renaldi.week06
 
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
