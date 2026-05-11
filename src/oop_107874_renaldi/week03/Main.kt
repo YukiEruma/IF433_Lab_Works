e5933fda078ab2cb4114706561ec0c4adfa0b0c1
Renaldi <renaldipanorama@gmail.com>
2026-05-11 14:27:47 +0700

    week03: test all encapsulation logic in main

--- src/oop_107874_renaldi/week03/Main.kt
+++ src/oop_107874_renaldi/week03/Main.kt
@@ -1,2 +1,17 @@
 package oop_107874_renaldi.week03
 
+fun main() {
+    val e = Employee("Budi")
+
+
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+
+    e.increasePerformance()
+
+
+
+    println("Pajak yang harus dibayar: ${e.tax}")
+}
