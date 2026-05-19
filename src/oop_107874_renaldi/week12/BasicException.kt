f36d25eb5946f0600024e6420c7b85746f2dd3c0
Renaldi <renaldipanorama@gmail.com>
2026-05-19 23:12:15 +0700

    week12: implement try as expression

--- src/oop_107874_renaldi/week12/BasicException.kt
+++ src/oop_107874_renaldi/week12/BasicException.kt
@@ -1,2 +1,25 @@
 package oop_107874_renaldi.week12
 
+fun divide(a: Int, b: Int): Int {
+    try {
+        return a / b
+    } catch (e: ArithmeticException) {
+        println("Error: ${e.message}")
+        return -1
+    } finally {
+        println("Division attempt finished")
+    }
+}
+
+fun main() {
+    println("=== TEST TRY AS EXPRESSION ===")
+    val inputString = "123A"
+
+    val result: Int = try {
+        Integer.parseInt(inputString)
+    } catch (e: NumberFormatException) {
+        -1
+    }
+
+    println("Hasil parsing: $result")
+}
