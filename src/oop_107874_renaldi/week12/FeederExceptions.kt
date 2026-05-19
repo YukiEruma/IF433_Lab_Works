2091a56396f8f15c58d060115c90242e7ed576c1
Renaldi <renaldipanorama@gmail.com>
2026-05-19 23:14:19 +0700

    week12: (task) create FeederException sealed class

--- src/oop_107874_renaldi/week12/FeederExceptions.kt
+++ src/oop_107874_renaldi/week12/FeederExceptions.kt
@@ -1,2 +1,10 @@
 package oop_107874_renaldi.week12
 
+
+sealed class FeederException(msg: String) : Exception(msg)
+
+class FoodEmptyException(requested: Int, available: Int) :
+    FeederException("Kibble tidak cukup! Diminta $requested gr, sisa $available gr")
+
+class DispenserJamException :
+    FeederException("Wadah dispenser tersangkut/macet!")
