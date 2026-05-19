5c9d2560029d1110ca654ac8c0fef3e20496cc1d
Renaldi <renaldipanorama@gmail.com>
2026-05-19 23:12:55 +0700

    week12: create InsufficientFunds exception

--- src/oop_107874_renaldi/week12/CustomExceptions.kt
+++ src/oop_107874_renaldi/week12/CustomExceptions.kt
@@ -1,2 +1,35 @@
 package oop_107874_renaldi.week12
 
+
+class InsufficientFundsException(
+    val amount: Double,
+    val balance: Double
+) : Exception("Attempted $amount, balance: $balance")
+
+class BankAccount(var balance: Double) {
+    fun withdraw(amount: Double) {
+        if (amount < 0) {
+            throw IllegalArgumentException("Amount must be positive")
+        }
+        if (amount > balance) {
+            throw InsufficientFundsException(amount, balance)
+        }
+        balance -= amount
+        println("Withdrawal successful. Remaining balance: $balance")
+    }
+}
+
+fun main() {
+    println("=== TEST MULTIPLE CATCH ===")
+    val account = BankAccount(100.0)
+
+    try {
+        account.withdraw(150.0) // Ini akan memicu InsufficientFundsException
+    } catch (e: InsufficientFundsException) {
+        println("Caught Domain Error: Uang tidak cukup. ${e.message}")
+    } catch (e: IllegalArgumentException) {
+        println("Caught Argument Error: Input tidak valid. ${e.message}")
+    } catch (e: Exception) {
+        println("Caught General Error: Terjadi kesalahan tidak terduga.")
+    }
+}
