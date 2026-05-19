d3f4289b8de59d76ebbc048be149c3538b27c507
Renaldi <renaldipanorama@gmail.com>
2026-05-19 23:13:39 +0700

    week12: implement basic runCatching

--- src/oop_107874_renaldi/week12/RunCatchingExample.kt
+++ src/oop_107874_renaldi/week12/RunCatchingExample.kt
@@ -1,2 +1,26 @@
 package oop_107874_renaldi.week12
 
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+
+
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
+
+
+    val safeValue = result.getOrElse { -1 }
+    println("Safe Value (getOrElse): $safeValue")
+
+    val recovered = result.recover { 0 }.getOrNull()
+    println("Recovered Value: $recovered")
+
+
+    runCatching {
+        "100".toInt()
+    }.onSuccess { v ->
+        println("Berhasil dikonversi: $v")
+    }.onFailure { e ->
+        println("Gagal konversi: ${e.message}")
+    }
+}
